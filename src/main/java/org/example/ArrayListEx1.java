c22e9608451c799345c4879eb235421aaeaf492c
Shahzad <shahzadtashkynov@gmail.com>
2025-03-26 23:06:46 +0500

    All files has been moved to ArrayListEx1 class

--- src/main/java/org/example/ArrayListEx1.java
+++ src/main/java/org/example/ArrayListEx1.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class ArrayListEx1 {
+    public static void main(String [] args){
+        ArrayList<String> names=new ArrayList<>(30);// Initilalized string arraylist
+        names.add("Ivan");
+        names.add("Peter");
+        System.out.println(names);
+        ArrayList <String> arrayList2=new ArrayList<>(names); //Initialize second arraylist with another arraylist
+        System.out.println(arrayList2);
+        arrayList2.add("Igor");
+        System.out.println(arrayList2);
+        for (String iter:arrayList2){// iterating in arraylist
+            System.out.println(iter);
+        }
+    }
 }
